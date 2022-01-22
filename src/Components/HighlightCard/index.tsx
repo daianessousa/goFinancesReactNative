2db5f6228735dc1b24c02247eb01f78417caf1a4
daianessousa <daisousa.c@gmail.com>
2022-01-22 18:28:46 -0300

    add cards styles

---
+++ src/Components/HighlightCard/index.tsx
@@ -0,0 +1,44 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+import {
+  Container,
+  Header,
+  Title,
+  Icon,
+  Footer,
+  Amount,
+  LastTransaction,
+} from "./styles";
+
+interface Props {
+  title: string;
+  amount: string;
+  lastTransaction: string;
+  type: "up" | "down" | "total";
+}
+
+export default function HighlightCard({
+  type,
+  title,
+  amount,
+  lastTransaction,
+}: Props) {
+  const icon = {
+    up: "arrow-up-circle",
+    down: "arrow-down-circle",
+    total: "dollar-sign",
+  };
+  return (
+    <Container type={type}>
+      <Header>
+        <Title type={type}>{title}</Title>
+        <Icon name={icon[type]} type={type} />
+      </Header>
+      <Footer>
+        <Amount type={type}>{amount}</Amount>
+        <LastTransaction type={type}>{lastTransaction}</LastTransaction>
+      </Footer>
+    </Container>
+  );
+}
---
+++ src/Components/TransactionCard/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Container,
+  Title,
+  Amount,
+  Footer,
+  Category,
+  Icon,
+  CategoryName,
+  Date,
+} from "./styles";
+
+interface Category {
+  name: string;
+  icon: string;
+}
+
+export interface TransactionsCardProps {
+  type: "positive" | "negative";
+  title: string;
+  amount: string;
+  category: Category;
+  date: string;
+}
+
+interface Props {
+  data: TransactionsCardProps;
+}
+
+export default function index({ data }: Props) {
+  return (
+    <Container>
+      <Title>{data.title}</Title>
+      <Amount type={data.type}>
+        {data.type === 'positive' ? '' : '- ' }
+        {data.amount}
+      </Amount>
+      <Footer>
+        <Category>
+          <Icon name={data.category.icon} />
+          <CategoryName>{data.category.name}</CategoryName>
+        </Category>
+        <Date>{data.date}</Date>
+      </Footer>
+    </Container>
+  );
+}
